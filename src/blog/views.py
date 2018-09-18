156705ff8079c13992e532d427915e37cb9ebd92
Deepak <raj.deeepak@gmail.com>
2018-09-18 14:28:22 +0530

    index page fixed

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Post
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'blog_list'
+    
+    def get_queryset(self):
+        """Return the all Posts"""
+        return Post.objects.all()
+    
